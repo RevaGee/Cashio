10e16844579520c51497a9a1d052f2b724ca4e74
RevaGee <revoryoutub@gmail.com>
2024-03-06 21:56:05 +0200

    Added telegram link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles.scss"
 import Logo from "../../images/CASHIO_logo_black.png"
-import {ArrowUp} from "../../images/icons/ArrowUp";
-import {ButtonCircle} from "../../images/icons/ButtonCircle";
-import {ButtonArrow} from "../../images/icons/ButtonArrow";
 
 
 export const Header = () => {
+    const handleTelegramLink = () => {
+        window.open('https://t.me/zheka_revor', '_blank');
+    };
+
     return(
         <div className="header">
             <div className = "left_side">
@@ -22,28 +23,26 @@ export const Header = () => {
                 </nav>
             </div>
             <div className="right_side">
-                <div>
-                    <ul className="nav-links">
-                        <li className="nav-link services">
-                            <a>EN
-                                <span className="material-icons dropdown-icon">
-                                    <ArrowUp/>
-                                </span>
-                            </a>
-                            <ul className="drop-down">
-                                <li>EN</li>
-                                <li>UA</li>
-                                <li>RU</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <button className="demo">
+                {/*<div>*/}
+                {/*    <ul className="nav-links">*/}
+                {/*        <li className="nav-link services">*/}
+                {/*            <a>EN*/}
+                {/*                <span className="material-icons dropdown-icon">*/}
+                {/*                    <ArrowUp/>*/}
+                {/*                </span>*/}
+                {/*            </a>*/}
+                {/*            <ul className="drop-down">*/}
+                {/*                <li>EN</li>*/}
+                {/*                <li>UA</li>*/}
+                {/*                <li>RU</li>*/}
+                {/*            </ul>*/}
+                {/*        </li>*/}
+                {/*    </ul>*/}
+                {/*</div>*/}
+                <button className="demo"
+                    onClick={handleTelegramLink}>
                     Get a free demo
-                    <div className = "button_icon">
-                        <div className = "button_circle"><ButtonCircle/></div>
-                        <div className = "button_arrow"><ButtonArrow/></div>
-                    </div>
+                    <div className = "button_icon"></div>
                 </button>
             </div>
         </div>
