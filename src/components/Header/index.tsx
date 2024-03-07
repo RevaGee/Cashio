9a3a33d6d121ccfd6e4d3c4f0f7b9ab8157a474a
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-07 20:21:13 +0200

    created burger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.scss"
 import Logo from "../../images/CASHIO_logo_black.png"
 import {ArrowUp} from "../../images/icons/ArrowUp";
 
 export const Header = () => {
+    const[open, isOpen] = useState(false);
+    const handleTelegramLink = () => {
+        window.open('https://t.me/zheka_revor', '_blank');
+    };
+    const handleClick = () => {
+        isOpen(!open);
+    };
     return(
         <div className="header">
             <div className = "left_side">
                 <div className = "logo"><img src={Logo} alt=""/></div>
-                <nav>
-                    <ul className="nav__links">
-                        <li><a href="#">FEATURES</a></li>
-                        <li><a href="#">WHY US</a></li>
-                        <li><a href="#">ABOUT US</a></li>
-                        <li><a href="#">PRICING</a></li>
-                        <li><a href="#">CONTACTS</a></li>
-                    </ul>
-                </nav>
+                <div>
+                    <nav className={open ? "show_menu" : "navigation"}>
+                        <ul className="nav__links">
+                            <li><a href="#">FEATURES</a></li>
+                            <li><a href="#">WHY US</a></li>
+                            <li><a href="#">ABOUT US</a></li>
+                            <li><a href="#">PRICING</a></li>
+                            <li><a href="#">CONTACTS</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
             <div className="right_side">
-                <div className = "language">
+                <div className="language">
                     <ul className="nav-links">
                         <li className="nav-link services">
                             <a>EN
@@ -35,9 +44,17 @@ export const Header = () => {
                         </li>
                     </ul>
                 </div>
-                <button className="demo">
+                <button className="demo" onClick={handleTelegramLink}>
                     Get a free demo
                 </button>
+                <label className = "burger">
+                    <input type = "checkbox"  name = ""/>
+                    <div className = "bar" onClick={handleClick}>
+                        <span className = "top"></span>
+                        <span className = "middle"></span>
+                        <span className = "bottom"></span>
+                    </div>
+                </label>
             </div>
         </div>
     );
