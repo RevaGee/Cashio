ac8db1c1917b553e017701dd5c6c7fc93d1bbbd9
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-07 09:34:40 +0200

    deleted unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <div style={{backgroundColor: "transparent", height: "300px"}}></div>
         <div style={{backgroundColor: "blue", height: "300px"}}></div>
         <div style={{backgroundColor: "transparent", height: "300px"}}></div>
+            <Footer/>
       </div>
   );
 }
