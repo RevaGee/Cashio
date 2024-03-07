69b341f445bf2b8f8a30517e848596eb2ba62d8b
RevaGee <revoryoutub@gmail.com>
2024-03-07 23:14:35 +0200

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
+import DarkMode from "./components/DarkMode/DarkMode";
 
 function App() {
   return (
       <div style={{width: "90%", margin:"0 auto"}} className="App">
         <Header/>
         <div style={{backgroundColor: "red", height: "300px"}}></div>
+          <DarkMode/>
         <div style={{backgroundColor: "transparent", height: "300px"}}></div>
         <div style={{backgroundColor: "yellow", height: "300px"}}></div>
         <div style={{backgroundColor: "transparent", height: "300px"}}></div>
