041459393c484a2b0a614d286c31bc43803d1d19
RevaGee <revoryoutub@gmail.com>
2024-03-13 10:33:24 +0200

    Added translates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,82 +1,97 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './styles.scss';
 import Logo from '../../images/CASHIO_logo_green.png';
-import { ArrowUp } from '../../images/icons/ArrowUp';
+import {ArrowUp} from '../../images/icons/ArrowUp';
 import DarkMode from "../DarkMode/DarkMode";
+import {useTranslation} from "react-i18next";
+import i18n from "../../translation/i18n";
 
 export const Header = () => {
-	const [open, isOpen] = useState(false);
-	const [language, isLanguage] = useState(false);
-	const handleTelegramLink = () => {
-		window.open('https://t.me/zheka_revor', '_blank');
-	};
-	const handleClick = () => {
-		isOpen(!open);
-	};
+    const [open, isOpen] = useState(false);
+    const [languages, isLanguages] = useState(false);
+
+    const {t} = useTranslation();
+
+
+    const handleLanguageChange = (language:string) => {
+        i18n.changeLanguage(language);
+        localStorage.setItem('language', language);
+    };
+
+
+
+    const handleTelegramLink = () => {
+        window.open('https://t.me/zheka_revor', '_blank');
+    };
+    const handleClick = () => {
+        isOpen(!open);
+    };
     const handleLanguageClick = () => {
-		isLanguage(!language);
-	};
-	return (
-		<div className={open ? 'mobile_header' : 'header'}>
-			<div className='left_side'>
-				<div className='logo'>
-					<img src={Logo} alt='' />
-				</div>
-				<div className = "nav_block">
-					<nav className={open ? 'show_menu' : 'navigation'}>
-						<ul className='nav__links'>
-							<li>
-								<a href='#'>Features</a>
-							</li>
-							<li>
-								<a href='#'>Why Us</a>
-							</li>
-							<li>
-								<a href='#'>About Us</a>
-							</li>
-							<li>
-								<a href='#'>Pricing</a>
-							</li>
-							<li>
-								<a href='#'>Contacts</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-			<div className='right_side'>
-				<div className='language'>
-					<ul className='nav-links'>
-						<li className='nav-link services' onClick={handleLanguageClick}>
-							<a>
-								EN
-								<span className='material-icons dropdown-icon' style={language?{transform:"rotate(180deg)"}:{}}>
-									<ArrowUp />
+        isLanguages(!languages);
+    };
+    return (
+        <div className={open ? 'mobile_header' : 'header'}>
+            <div className='left_side'>
+                <div className='logo'>
+                    <img src={Logo} alt=''/>
+                </div>
+                <div className="nav_block">
+                    <nav className={open ? 'show_menu' : 'navigation'}>
+                        <ul className='nav__links'>
+                            <li>
+                                <a href='#'>{t('Features')}</a>
+                            </li>
+                            <li>
+                                <a href='#'>{t('Why Us')}</a>
+                            </li>
+                            <li>
+                                <a href='#'>{t('About Us')}</a>
+                            </li>
+                            <li>
+                                <a href='#'>{t('Pricing')}</a>
+                            </li>
+                            <li>
+                                <a href='#'>{t('Contacts')}</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div className='right_side'>
+                <div className='language'>
+                    <ul className='nav-links'>
+                        <li className='nav-link services' onClick={handleLanguageClick}>
+                            <a>
+                                EN
+                                <span className='material-icons dropdown-icon'
+                                      style={languages ? {transform: "rotate(180deg)"} : {}}>
+									<ArrowUp/>
 								</span>
-							</a>
-							<ul className='drop-down' style={language?{display:'block',justifyContent:'center'}:{}}>
-								<li>EN</li>
-								<div className='location-line'></div>
-								<li>UA</li>
-								<div className='location-line'></div>
-								<li>RU</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-				<button className='demo' onClick={handleTelegramLink}>
-					Get a free demo
-				</button>
-				<DarkMode/>
-				<label className='burger'>
-					<input type='checkbox' name='' />
-					<div className='bar' onClick={handleClick}>
-						<span className='top'></span>
-						<span className='middle'></span>
-						<span className='bottom'></span>
-					</div>
-				</label>
-			</div>
-		</div>
-	);
+                            </a>
+                            <ul className='drop-down'
+                                style={languages ? {display: 'block', justifyContent: 'center'} : {}}>
+                                <li onClick={() => handleLanguageChange('en')} >EN</li>
+                                <div className='location-line'></div>
+                                <li onClick={() => handleLanguageChange('ua')} >UA</li>
+                                <div className='location-line'></div>
+                                <li onClick={() => handleLanguageChange('ru')} >RU</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <button className='demo' onClick={handleTelegramLink}>
+                    {t('Get a free demo')}
+                </button>
+                <DarkMode/>
+                <label className='burger'>
+                    <input type='checkbox' name=''/>
+                    <div className='bar' onClick={handleClick}>
+                        <span className='top'></span>
+                        <span className='middle'></span>
+                        <span className='bottom'></span>
+                    </div>
+                </label>
+            </div>
+        </div>
+    );
 };
---
+++ src/translation/i18n.js
@@ -0,0 +1,30 @@
+import en from './en.json'
+import ru from './ru.json'
+import ua from './ua.json'
+
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+
+const resources = {
+    en: {
+        translation: en,
+    },
+    ru: {
+        translation: ru,
+    },
+    ua: {
+        translation: ua,
+    }
+}
+
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: localStorage.getItem('language') || 'en',
+        fallbackLng: 'en'
+    })
+
+
+export default i18n;
