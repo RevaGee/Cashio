be1f73437fa5608eb64be5bad2b5d8174d7c295b
RevaGee <revoryoutub@gmail.com>
2024-03-13 10:50:51 +0200

    Fixed button translates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,17 +9,18 @@ import i18n from "../../translation/i18n";
 export const Header = () => {
     const [open, isOpen] = useState(false);
     const [languages, isLanguages] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+    const [selectedLanguage, setSelectedLanguage] = useState(localStorage.getItem('language') || 'en');
+
 
     const {t} = useTranslation();
 
 
-    const handleLanguageChange = (language:string) => {
+    const handleLanguageChange = (language: string) => {
+        setSelectedLanguage(language);
         i18n.changeLanguage(language);
         localStorage.setItem('language', language);
     };
-
-
-
     const handleTelegramLink = () => {
         window.open('https://t.me/zheka_revor', '_blank');
     };
@@ -29,6 +30,8 @@ export const Header = () => {
     const handleLanguageClick = () => {
         isLanguages(!languages);
     };
+
+
     return (
         <div className={open ? 'mobile_header' : 'header'}>
             <div className='left_side'>
@@ -62,7 +65,7 @@ export const Header = () => {
                     <ul className='nav-links'>
                         <li className='nav-link services' onClick={handleLanguageClick}>
                             <a>
-                                EN
+                                {selectedLanguage.toUpperCase()}
                                 <span className='material-icons dropdown-icon'
                                       style={languages ? {transform: "rotate(180deg)"} : {}}>
 									<ArrowUp/>
@@ -70,11 +73,23 @@ export const Header = () => {
                             </a>
                             <ul className='drop-down'
                                 style={languages ? {display: 'block', justifyContent: 'center'} : {}}>
-                                <li onClick={() => handleLanguageChange('en')} >EN</li>
+                                <li onClick={() => {
+                                    handleLanguageChange('en');
+                                    setIsActive(isActive);
+                                }}>EN
+                                </li>
                                 <div className='location-line'></div>
-                                <li onClick={() => handleLanguageChange('ua')} >UA</li>
+                                <li onClick={() => {
+                                    handleLanguageChange('ua');
+                                    setIsActive(isActive);
+                                }}>UA
+                                </li>
                                 <div className='location-line'></div>
-                                <li onClick={() => handleLanguageChange('ru')} >RU</li>
+                                <li onClick={() => {
+                                    handleLanguageChange('ru');
+                                    setIsActive(isActive);
+                                }}>RU
+                                </li>
                             </ul>
                         </li>
                     </ul>
