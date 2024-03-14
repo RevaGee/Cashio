418d357c7d2d425ccfd65dbddcff08d89f2d1167
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-14 21:45:45 +0200

    added particles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
 import ArrowScroll  from "./components/Arrow";
+import ParticlesComponent from "./components/Paralax/particles";
+
 
 function App() {
     return (
         <div style={{width: "100%", margin: "0 auto"}} className="App">
             <Header/>
+            <ParticlesComponent/>
             <div style={{backgroundColor: "red", height: "300px", width: "90%", margin: "0 auto"}}></div>
             <div style={{backgroundColor: "transparent", height: "300px", width: "90%", margin: "0 auto"}}></div>
             <div style={{backgroundColor: "yellow", height: "300px", width: "90%", margin: "0 auto"}}></div>
