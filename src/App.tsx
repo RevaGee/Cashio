713be57e1b1f84939cded4a0dd720e2db0b271e9
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-18 01:21:59 +0200

    added collages

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,16 @@ import {Footer} from "./components/Footer";
 import ArrowScroll from "./components/Arrow";
 import ParticlesComponent from "./components/Paralax/particles";
 import {Slogan} from "./components/Slogan";
+import {Collages} from "./components/Collages";
 
 
 function App() {
     return (
         <div style={{width: "100%", margin: "0 auto"}} className="App">
             <Header/>
-            <ParticlesComponent/>
+            {/*<ParticlesComponent/>*/}
             <Slogan/>
-            <div style={{backgroundColor: "transparent", height: "300px", width: "90%", margin: "0 auto"}}></div>
-            <div style={{backgroundColor: "yellow", height: "300px", width: "90%", margin: "0 auto"}}></div>
-            <div style={{backgroundColor: "transparent", height: "300px", width: "90%", margin: "0 auto"}}></div>
-            <div style={{backgroundColor: "blue", height: "300px", width: "90%", margin: "0 auto"}}></div>
-            <div style={{backgroundColor: "transparent", height: "300px", width: "90%", margin: "0 auto"}}></div>
-            <div style={{backgroundColor: "red", height: "300px", width: "90%", margin: "0 auto"}}></div>
-            <div style={{backgroundColor: "transparent", height: "300px", width: "90%", margin: "0 auto"}}></div>
-            <div style={{backgroundColor: "yellow", height: "300px", width: "90%", margin: "0 auto"}}></div>
+            <Collages/>
             <div style={{backgroundColor: "transparent", height: "300px", width: "90%", margin: "0 auto"}}></div>
             <div style={{backgroundColor: "blue", height: "300px", width: "90%", margin: "0 auto"}}></div>
             <div style={{backgroundColor: "transparent", height: "300px", width: "90%", margin: "0 auto"}}></div>
