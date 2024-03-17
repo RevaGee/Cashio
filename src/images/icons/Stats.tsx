9bac6097ff545413640ddd4c730666975993a0f7
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-17 23:30:49 +0200

    added svgs to collages

---
+++ src/images/icons/Stats.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export const CollageSvg = () => {
+    return (
+        <svg width="74" height="74" viewBox="0 0 74 74" fill="#1FA84F" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd"
+                  d="M46.2503 24.6668H58.5837V61.6668H15.417V30.8335H27.7503V12.3335H46.2503V24.6668ZM40.0837 18.5002H33.917V55.5002H40.0837V18.5002ZM46.2503 30.8335V55.5002H52.417V30.8335H46.2503ZM27.7503 37.0002V55.5002H21.5837V37.0002H27.7503Z"
+                  fill="url(#paint0_linear_153_13184)"/>
+            <defs>
+                <linearGradient id="paint0_linear_153_13184" x1="-5.72958" y1="37.0947" x2="44.4905" y2="82.2573"
+                                gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#1FA84F"/>
+                    <stop offset="1" stopColor="#1FA84F"/>
+                    <stop offset="1" stopColor="#1FA84F"/>
+                </linearGradient>
+            </defs>
+        </svg>
+    );
+};
