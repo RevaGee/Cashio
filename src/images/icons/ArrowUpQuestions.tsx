eb44a39ffdfcb7050ad662290eb3a7c88a9c3a7a
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-19 12:39:42 +0200

    added ArrowUpQuestions svg

---
+++ src/images/icons/ArrowUpQuestions.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const ArrowUpQuestions = () => {
+    return (
+        <svg height="24" id="Layer_1"  version="1.1" viewBox="0 0 512 512" width="24" fill="#1FA84F"><polygon points="396.6,352 416,331.3 256,160 96,331.3 115.3,352 256,201.5 "/></svg>
+    );
+};
