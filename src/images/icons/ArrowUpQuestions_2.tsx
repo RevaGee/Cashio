699fb50cd511a5d6b87d85cd751ea109581e416c
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-20 12:53:18 +0200

    changed questions arrow icon

---
+++ src/images/icons/ArrowUpQuestions_2.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const ArrowUpQuestions_2 = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="#1FA84F"
+             className="bi bi-chevron-compact-down" viewBox="0 0 24 24">
+            <path fillRule="evenodd"
+                  d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67"/>
+        </svg>
+    );
+};
