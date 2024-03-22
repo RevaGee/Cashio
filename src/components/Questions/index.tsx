f88842959f4a68273b597a3b57c98fa93703d446
RevaGee <revoryoutub@gmail.com>
2024-03-22 21:57:50 +0200

    added animations for questions

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,11 +1,22 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 import "./styles.scss";
 import QuestionLogo from "../../images/customer.png";
-import {ArrowUpQuestions_2} from "../../images/icons/ArrowUpQuestions_2";
+import { ArrowUpQuestions_2 } from "../../images/icons/ArrowUpQuestions_2";
+import { useInView } from "react-intersection-observer";
 
 export const Questions = () => {
     const [openIndexes, setOpenIndexes] = useState<number[]>([]);
     const [arrowRotatedIndexes, setArrowRotatedIndexes] = useState<number[]>([]);
+
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
 
     const toggleQuestion = (index: number) => {
         const isArrowRotated = arrowRotatedIndexes.includes(index);
@@ -20,60 +31,96 @@ export const Questions = () => {
     };
 
     return (
-        <div className = 'faq'>
-            <div className = 'heading'>
+        <motion.div
+            className="faq"
+            initial="hidden"
+            animate={controls}
+            variants={{
+                visible: { opacity: 1 },
+                hidden: { opacity: 0 },
+            }}
+            transition={{ duration: 0.5 }}
+        >
+            <div className="heading">
                 <h4>FAQ</h4>
                 <h1>Frequently Asked Questions</h1>
                 <p>
-                    As A Matter Of Fact The Unification Of The Coherent Software Provides A Strict Control Over
-                    The Accomplishment Of Intended Estimation
+                    As A Matter Of Fact The Unification Of The Coherent Software Provides
+                    A Strict Control Over The Accomplishment Of Intended Estimation
                 </p>
             </div>
             <div className="questions">
                 <div className="left_content">
-                    <img src={QuestionLogo} alt="" className="logo_faq"/>
+                    <motion.img
+                        src={QuestionLogo}
+                        alt=""
+                        className="logo_faq"
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5 }}
+                    />
                 </div>
                 <div className="right_content">
-                    <div className="box">
+                    <div className="box" ref={ref}>
                         {[
                             "Какие способы оплаты?",
                             "Если я не смогу заплатить вовремя, потеряю ли я данные?",
                             "Какие ваши преимущества в сравнении с конкурентами?",
-                            "Есть ли какие-то ограничения в использовании?"
+                            "Есть ли какие-то ограничения в использовании?",
                         ].map((question, index) => (
-                            <div
-                                key={index} className="question_content"
-                                style={
-                                    arrowRotatedIndexes.includes(index) ?
-                                        {maxHeight: "150px", transition: "max-height 0.3s"} :
-                                        {maxHeight: "90px"}
-                                }
+                            <motion.div
+                                key={index}
+                                className="question_content"
+                                style={{
+                                    maxHeight: openIndexes.includes(index) ? "150px" : "90px",
+                                }}
                                 onClick={() => toggleQuestion(index)}
+                                initial="hidden"
+                                animate={controls}
+                                variants={{
+                                    visible: { opacity: 1, y: 0 },
+                                    hidden: { opacity: 0, y: -20 },
+                                }}
+                                transition={{ duration: 0.3, delay: index * 0.1 }}
                             >
-                                <div className="question">
+                                <motion.div
+                                    className="question"
+                                    initial={{ opacity: 0, y: -20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    transition={{ duration: 0.5 }}
+                                >
                                     <p>{question}</p>
-                                    <div className = 'question_svg'
-                                         style={arrowRotatedIndexes.includes(index) ?
-                                             { transform: "rotate(180deg)", transition: " transform 0.3s ease" } :
-                                             {transform: "rotate(0deg)", transition: " transform 0.3s ease"}}>
+                                    <motion.div
+                                        className="question_svg"
+                                        animate={{
+                                            rotate: arrowRotatedIndexes.includes(index) ? 180 : 0,
+                                        }}
+                                    >
                                         <ArrowUpQuestions_2 />
-                                    </div>
-                                </div>
-                                <div className="answer" >
+                                    </motion.div>
+                                </motion.div>
+                                <motion.div
+                                    className="answer"
+                                    initial={{ opacity: 0, y: -20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    transition={{ duration: 0.5 }}
+                                >
                                     {openIndexes.includes(index) && (
                                         <p>
                                             {index === 0 && "Для общего удобства используется USDT."}
-                                            {index === 1 && "Нет, Вам будет выделен буферный период, для этого свяжитесь с Вашим менеджером."}
-                                            {index === 2 && "Мы знаем на практике и с какими проблемами сталкиваются арбитражные команды и партнёрские программы и решили их с помощью нашего продукта."}
+                                            {index === 1 &&
+                                                "Нет, Вам будет выделен буферный период, для этого свяжитесь с Вашим менеджером."}
+                                            {index === 2 &&
+                                                "Мы знаем на практике и с какими проблемами сталкиваются арбитражные команды и партнёрские программы и решили их с помощью нашего продукта."}
                                             {index === 3 && "Нет. Ответственность за использование за Вами."}
                                         </p>
                                     )}
-                                </div>
-                            </div>
+                                </motion.div>
+                            </motion.div>
                         ))}
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 };
