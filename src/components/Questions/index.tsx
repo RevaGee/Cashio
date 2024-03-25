cee3bd045e53fc57ee8df0a6c72945b746c80d41
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-25 19:35:30 +0200

    corrected questions media

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -33,98 +33,100 @@ export const Questions = () => {
 
     return (
         <div className='faq'>
-            <motion.div
-                ref={ref}
-                className='heading'
-                animate={{opacity: inView ? 1 : 0, x: inView ? 0 : -200}}
-                transition={{duration: 0.5}}>
-                <h4>{t('heading.heading1')}</h4>
-                <h1> {t('heading.heading2')}</h1>
-                <p>
-                    {t('heading.heading3')}
-                </p>
-            </motion.div>
-            <div className="questions">
-                <div className="left_content">
-                    <motion.img
-                        ref={ref_2}
-                        src={QuestionLogo}
-                        alt=""
-                        className="logo_faq"
-                        animate={{opacity: inView_2 ? 1 : 0, y: inView_2 ? 0 : -100}}
-                        transition={{duration: 0.5}}
-                    />
-                </div>
-                <div className="right_content">
-                    <div className='box' ref={ref_3}>
-                        {[
-                            t('questions.question1'),
-                            t('questions.question2'),
-                            t('questions.question3'),
-                            t('questions.question4')
-                        ].map((question, index) => (
-                            <motion.div
-                                key={index}
-                                className="question_content"
-                                style={{
-                                    maxHeight: openIndexes.includes(index) ? "150px" : "90px",
-                                }}
-                                onClick={() => toggleQuestion(index)}
-                                initial={{opacity: 0, y: 300}}
-                                animate={{opacity: inView_3 ? 1 : 0, y: inView_3 ? 0 : 300}}
-                                transition={{duration: 0.5, delay: index * 0.3}}
-                                variants={{
-                                    visible: {
-                                        opacity: 1,
-                                        y: 0,
-                                        transition: {duration: 0.5},
-                                    },
-                                    hidden: {
-                                        opacity: 0,
-                                        y: -20,
-                                        transition: {duration: 0.5},
-                                    },
-                                }}
-                                //whileHover={{scale: 1.05}}
-                                //whileTap={{scale: 0.95}}
-                            >
+            <div className = 'faq_media'>
+                <motion.div
+                    ref={ref}
+                    className='heading'
+                    animate={{opacity: inView ? 1 : 0, x: inView ? 0 : -200}}
+                    transition={{duration: 0.5}}>
+                    <h4>{t('heading.heading1')}</h4>
+                    <h1> {t('heading.heading2')}</h1>
+                    <p>
+                        {t('heading.heading3')}
+                    </p>
+                </motion.div>
+                <div className="questions">
+                    <div className="left_content">
+                        <motion.img
+                            ref={ref_2}
+                            src={QuestionLogo}
+                            alt=""
+                            className="logo_faq"
+                            animate={{opacity: inView_2 ? 1 : 0, y: inView_2 ? 0 : -100}}
+                            transition={{duration: 0.5}}
+                        />
+                    </div>
+                    <div className="right_content">
+                        <div className='box' ref={ref_3}>
+                            {[
+                                t('questions.question1'),
+                                t('questions.question2'),
+                                t('questions.question3'),
+                                t('questions.question4')
+                            ].map((question, index) => (
                                 <motion.div
-                                    className="question"
-                                    initial={{opacity: 0, y: -20}}
-                                    animate={{opacity: 1, y: 0}}
-                                    transition={{duration: 0.3}}
+                                    key={index}
+                                    className="question_content"
+                                    style={{
+                                        maxHeight: openIndexes.includes(index) ? "150px" : "90px",
+                                    }}
+                                    onClick={() => toggleQuestion(index)}
+                                    initial={{opacity: 0, y: 300}}
+                                    animate={{opacity: inView_3 ? 1 : 0, y: inView_3 ? 0 : 300}}
+                                    transition={{duration: 0.5, delay: index * 0.3}}
+                                    variants={{
+                                        visible: {
+                                            opacity: 1,
+                                            y: 0,
+                                            transition: {duration: 0.5},
+                                        },
+                                        hidden: {
+                                            opacity: 0,
+                                            y: -20,
+                                            transition: {duration: 0.5},
+                                        },
+                                    }}
+                                    //whileHover={{scale: 1.05}}
+                                    //whileTap={{scale: 0.95}}
                                 >
-                                    <p>{question}</p>
                                     <motion.div
-                                        className="question_svg"
+                                        className="question"
+                                        initial={{opacity: 0, y: -20}}
+                                        animate={{opacity: 1, y: 0}}
+                                        transition={{duration: 0.3}}
+                                    >
+                                        <p>{question}</p>
+                                        <motion.div
+                                            className="question_svg"
+                                            animate={{
+                                                rotate: arrowRotatedIndexes.includes(index) ? 180 : 0,
+                                                transition: {duration: 0.3},
+                                            }}
+                                        >
+                                            <ArrowUpQuestions_2/>
+                                        </motion.div>
+                                    </motion.div>
+                                    <motion.div
+                                        className="answer"
+                                        initial={{opacity: 0, y: -20}}
                                         animate={{
-                                            rotate: arrowRotatedIndexes.includes(index) ? 180 : 0,
+                                            opacity: openIndexes.includes(index) ? 1 : 0,
+                                            y: openIndexes.includes(index) ? 0 : -20,
                                             transition: {duration: 0.3},
                                         }}
                                     >
-                                        <ArrowUpQuestions_2/>
+                                        {openIndexes.includes(index) && (
+                                            <p>
+                                                {index === 0 && t('answer.answer1')}
+                                                {index === 1 && t('answer.answer2')}
+                                                {index === 2 && t('answer.answer3')}
+                                                {index === 3 && t('answer.answer4')}
+                                            </p>
+                                        )}
                                     </motion.div>
                                 </motion.div>
-                                <motion.div
-                                    className="answer"
-                                    initial={{opacity: 0, y: -20}}
-                                    animate={{
-                                        opacity: openIndexes.includes(index) ? 1 : 0,
-                                        y: openIndexes.includes(index) ? 0 : -20,
-                                        transition: {duration: 0.3},
-                                    }}
-                                >
-                                    {openIndexes.includes(index) && (
-                                        <p>
-                                            {index === 0 && t('answer.answer1')}
-                                            {index === 1 && t('answer.answer2')}
-                                            {index === 2 && t('answer.answer3')}
-                                            {index === 3 && t('answer.answer4')}
-                                        </p>
-                                    )}
-                                </motion.div>
-                            </motion.div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
