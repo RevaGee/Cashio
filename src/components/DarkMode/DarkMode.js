ee6eaab5fe195f9e43e3bde2f0b511a11b4abc53
RevaGee <revoryoutub@gmail.com>
2024-03-28 00:27:15 +0200

    State for dark-mode

--- src/components/DarkMode/DarkMode.js
+++ src/components/DarkMode/DarkMode.js
@@ -1,29 +1,43 @@
+import React, { useEffect } from 'react';
 import { ReactComponent as Sun } from './Sun.svg';
 import { ReactComponent as Moon } from './Moon.svg';
 import './DarkMode.css';
 
-const DarkMode = () => {
-	const setDarkMode = () => {
-		document.querySelector('body').setAttribute('data-theme', 'dark');
+/**
+ * @typedef {Object} DarkModeProps
+ * @property {boolean} darkMode - Флаг, указывающий, включен ли темный режим.
+ * @property {function} toggleDarkMode - Функция для переключения темного режима.
+ */
+
+/**
+ * Компонент DarkMode.
+ * @param {DarkModeProps} props - Свойства компонента.
+ */
+const DarkMode = ({ darkMode, toggleDarkMode }) => {
+	useEffect(() => {
+		if (darkMode) {
+			document.querySelector('body').setAttribute('data-theme', 'dark');
+		} else {
+			document.querySelector('body').setAttribute('data-theme', 'light');
+		}
+	}, [darkMode]);
+
+	const toggleTheme = () => {
+		toggleDarkMode();
 	};
-	const setLightMode = () => {
-		document.querySelector('body').setAttribute('data-theme', 'light');
-	};
-	const toggleTheme = e => {
-		if (e.target.checked) setDarkMode();
-		else setLightMode();
-	};
+
 	return (
-		<div className='dark_mode'>
+		<div className="dark_mode">
 			<input
-				className='dark_mode_input'
-				type='checkbox'
-				id='darkmode-toggle'
+				className="dark_mode_input"
+				type="checkbox"
+				id="darkmode-toggle"
+				checked={darkMode}
 				onChange={toggleTheme}
 			/>
-			<label className='dark_mode_label' for='darkmode-toggle'>
-				<Sun className = "sun"/>
-				<Moon className = "moon"/>
+			<label className="dark_mode_label" htmlFor="darkmode-toggle">
+				<Sun className="sun" />
+				<Moon className="moon" />
 			</label>
 		</div>
 	);
