d79633eb919f73162a44bbb171fbebbbac8ce032
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-03-28 20:07:13 +0200

    corrected pricing component

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -10,48 +10,57 @@ export const Pricing = () => {
     };
 
     return (
-        <div className='pricing_content'>
-            <div className='overlays_box'>
-                <div className='under_head_pricing'>
-                    <h1>$650</h1>
+        <div className = 'pricing'>
+            <div className = 'pricing_media'>
+                <div className = 'pricing_text'>
+                    <h4>AFFORDABLE SOLUTION, UNMATCHED VALUE</h4>
+                    <h1>CHOOSE YOUR PLAN</h1>
+                    <p>Transparent pricing plans with no surprise fees</p>
                 </div>
-                <div className='month'>
-                    <p> {t('pricing.month1')}</p>
+                <div className='pricing_content'>
+                    <div className='overlays_box'>
+                        <div className='under_head_pricing'>
+                            <h1>$650</h1>
+                        </div>
+                        <div className='month'>
+                            <p> {t('pricing.month1')}</p>
+                        </div>
+                        <div className='under_description_pricing'>
+                            <p> {t('pricing.billed1')}</p>
+                        </div>
+                        <button className='demo_contacts_pricing' onClick={handleTelegramLink}>
+                            {t('Get a free demo')}
+                        </button>
+                    </div>
+                    <div className='overlays_box_3'>
+                        <div className='under_head_pricing_3'>
+                            <h1>$1700</h1>
+                        </div>
+                        <div className='month_3'>
+                            <p> {t('pricing.month3')}</p>
+                        </div>
+                        <div className='under_description_pricing_3'>
+                            <p> {t('pricing.billed3')}</p>
+                        </div>
+                        <button className='demo_contacts_pricing' onClick={handleTelegramLink}>
+                            {t('Get a free demo')}
+                        </button>
+                    </div>
+                    <div className='overlays_box'>
+                        <div className='under_head_pricing'>
+                            <h1>$2900</h1>
+                        </div>
+                        <div className='month'>
+                            <p> {t('pricing.month6')}</p>
+                        </div>
+                        <div className='under_description_pricing'>
+                            <p> {t('pricing.billed6')}</p>
+                        </div>
+                        <button className='demo_contacts_pricing' onClick={handleTelegramLink}>
+                            {t('Get a free demo')}
+                        </button>
+                    </div>
                 </div>
-                <div className='under_description_pricing'>
-                    <p> {t('pricing.billed1')}</p>
-                </div>
-                <button className='demo_contacts_pricing' onClick={handleTelegramLink}>
-                    {t('Get a free demo')}
-                </button>
-            </div>
-            <div className='overlays_box_3'>
-                <div className='under_head_pricing_3'>
-                    <h1>$1700</h1>
-                </div>
-                <div className='month_3'>
-                    <p> {t('pricing.month3')}</p>
-                </div>
-                <div className='under_description_pricing_3'>
-                    <p> {t('pricing.billed3')}</p>
-                </div>
-                <button className='demo_contacts_pricing' onClick={handleTelegramLink}>
-                    {t('Get a free demo')}
-                </button>
-            </div>
-            <div className='overlays_box'>
-                <div className='under_head_pricing'>
-                    <h1>$2900</h1>
-                </div>
-                <div className='month'>
-                    <p> {t('pricing.month6')}</p>
-                </div>
-                <div className='under_description_pricing'>
-                    <p> {t('pricing.billed6')}</p>
-                </div>
-                <button className='demo_contacts_pricing' onClick={handleTelegramLink}>
-                    {t('Get a free demo')}
-                </button>
             </div>
         </div>
     );
