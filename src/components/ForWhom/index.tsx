2f8c6692dae9f9a143a474cd02278da1a6c2c0b7
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-04-01 17:14:25 +0300

    created for whom animations

--- src/components/ForWhom/index.tsx
+++ src/components/ForWhom/index.tsx
@@ -17,16 +17,49 @@ import {PokerTipSvg} from '../../images/icons/PokerTipSvg'
 import {CartSvg} from '../../images/icons/CartSvg'
 import {ArrowRight} from "../../images/icons/ArrowRight";
 import {BitcoinSvg} from "../../images/icons/BitcoinSvg";
+import { motion } from "framer-motion";
+import {useInView} from "react-intersection-observer";
 export const ForWhom = () => {
+    const [ref_for_whom_text, inView_for_whom_text] = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+    const [ref_for_whom_1, inView_for_whom_1] = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+    const [ref_for_whom_2, inView_for_whom_2] = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+    const [ref_for_whom_3, inView_for_whom_3] = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+    const [ref_for_whom_4, inView_for_whom_4] = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
     return(
         <div className = 'for_whom'>
             <div className = 'for_whom_media'>
-                <div className = 'for_whom_text'>
+                <motion.div
+                    ref={ref_for_whom_text}
+                    className = 'for_whom_text'
+                    initial={{opacity: 0, x: 0}}
+                    animate={{opacity: inView_for_whom_text ? 1 : 0, x: inView_for_whom_text ? 0 : -200}}
+                    transition={{duration: 1}}
+                >
                     <h1>FOR WHOM</h1>
-                </div>
+                </motion.div>
                 <div className='for_whom_blocks'>
-                    <div className='block_crypto block'>
-                        <div className = 'arrow_block'><ArrowRight/></div>
+                    <motion.div
+                        ref={ref_for_whom_1}
+                        className='block_crypto block'
+                        initial={{opacity: 0, y: 0}}
+                        animate={{opacity: inView_for_whom_1 ? 1 : 0, y: inView_for_whom_1 ? 0 : 200}}
+                        transition={{duration: 1}}
+                    >
                         <BitcoinSvg/>
                         <h4>Crypto</h4>
                         <div className='block_img'>
@@ -34,9 +67,14 @@ export const ForWhom = () => {
                             <div className='img_bg'><img src={Facebook} alt=''/></div>
                             <div className='img_bg'><img src={GoogleAds} alt=''/></div>
                         </div>
-                    </div>
-                    <div className='block_crypto block'>
-                        <div className='arrow_block'><ArrowRight/></div>
+                    </motion.div>
+                    <motion.div
+                        ref={ref_for_whom_2}
+                        className='block_crypto block'
+                        initial={{opacity: 0, y: 0}}
+                        animate={{opacity: inView_for_whom_2 ? 1 : 0, y: inView_for_whom_2 ? 0 : 200}}
+                        transition={{duration: 1.2}}
+                    >
                         <HeartSvg/>
                         <h4>Dating</h4>
                         <div className='block_img'>
@@ -44,9 +82,14 @@ export const ForWhom = () => {
                             <div className='img_bg'><img src={TikTok} alt=''/></div>
                             <div className='img_bg'><img src={Taboola} alt=''/></div>
                         </div>
-                    </div>
-                    <div className='block_crypto block'>
-                        <div className='arrow_block'><ArrowRight/></div>
+                    </motion.div>
+                    <motion.div
+                        ref={ref_for_whom_3}
+                        className='block_crypto block'
+                        initial={{opacity: 0, y: 0}}
+                        animate={{opacity: inView_for_whom_3 ? 1 : 0, y: inView_for_whom_3 ? 0 : 200}}
+                        transition={{duration: 1.4}}
+                    >
                         <PokerTipSvg/>
                         <h4>Igaming</h4>
                         <div className='block_img'>
@@ -54,9 +97,14 @@ export const ForWhom = () => {
                             <div className='img_bg'><img src={Snapchat} alt=''/></div>
                             <div className='img_bg'><img src={Bing} alt=''/></div>
                         </div>
-                    </div>
-                    <div className='block_crypto block'>
-                        <div className='arrow_block'><ArrowRight/></div>
+                    </motion.div>
+                    <motion.div
+                        ref={ref_for_whom_4}
+                        className='block_crypto block'
+                        initial={{opacity: 0, y: 0}}
+                        animate={{opacity: inView_for_whom_4 ? 1 : 0, y: inView_for_whom_4 ? 0 : 200}}
+                        transition={{duration: 1.6}}
+                    >
                         <CartSvg/>
                         <h4>E-commerce</h4>
                         <div className='block_img'>
@@ -64,7 +112,7 @@ export const ForWhom = () => {
                             <div className='img_bg'><img src={Amazon} alt=''/></div>
                             <div className='img_bg'><img src={Linkedin} alt=''/></div>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
