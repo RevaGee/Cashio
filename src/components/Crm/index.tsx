1777d94f44a3f776ad773fb903ceda73244b163e
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-04-03 17:45:48 +0300

    corrected crm component

--- src/components/Crm/index.tsx
+++ src/components/Crm/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './styles.scss'
-import CRM from '../../images/icons/Crm.png'
+import '../../index.css'
+import CRM_black from '../../images/icons/Crm_black.png'
+import CRM_grey from '../../images/icons/Crm_grey.png'
 export const Crm = () => {
-    return(
-        <div className = 'crm'>
-            <div className = 'crm_media'>
+    useEffect(() => {
+        localStorage.getItem('darkMode');
+    }, [localStorage.getItem('darkMode')]);
+    return (
+        <div className='crm'>
+            <div className='crm_media'>
                 <h1>Optimize <span>non-stop</span><br/>Check your profits
-                    <span className = 'text_ml'>in a minute</span></h1>
-                <img src={CRM} alt=''/>
+                    <span className='text_ml'>in a minute</span></h1>
+                <img src={localStorage.getItem('darkMode') ? CRM_grey : CRM_black} alt=''/>
             </div>
         </div>
     );
