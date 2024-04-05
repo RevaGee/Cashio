ee936618e6e6f2f50cf54aa113212da985d1dc59
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-04-05 15:30:13 +0300

    corrected crm component

--- src/components/Crm/index.tsx
+++ src/components/Crm/index.tsx
@@ -12,7 +12,7 @@ import { useTheme } from "../DarkMode";
 
 export const Crm = () => {
     const { darkMode } = useTheme();
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         const handleResize = () => {
@@ -27,13 +27,13 @@ export const Crm = () => {
     return (
         <div className='crm'>
             <div className='crm_media'>
-                <ContainerScroll 
+                <ContainerScroll
                     titleComponent={
                         <h1>Optimize <span>non-stop</span><br/>Check your profits
                             <span className='text_ml'>in a minute</span></h1>
                     }
                 >
-                    {windowWidth < 768 ? (
+                    {/*{windowWidth < 768 ? (
                         darkMode ? (
                             <img src={CRM_grey_small} alt=''/>
                     ) : (
@@ -45,7 +45,12 @@ export const Crm = () => {
                     ) : (
                         <img src={CRM_black} alt=''/>
                     )
-                )}
+                )}*/}
+                    <img className = 'lol_small' src={CRM_grey_small} alt='' style={windowWidth < 768 ? (darkMode ? {display: 'flex'} : {display: 'none'}):{display: 'none'}}/>
+                    <img className = 'lol_small' src={CRM_black_small} alt='' style={windowWidth < 768 ? (darkMode ? {display: 'none'} : {display: 'flex'}):{display: 'none'}}/>
+                    <img className = 'lol' src={CRM_grey} alt='' style={windowWidth > 768 ? (darkMode ? {display: 'flex'} : {display: 'none'}):{display: 'none'}}/>
+                    <img className = 'lol' src={CRM_black} alt='' style={windowWidth > 768 ? (darkMode ? {display: 'none'} : {display: 'flex'}):{display: 'none'}}/>
+
                 </ContainerScroll>
             </div>
         </div>
