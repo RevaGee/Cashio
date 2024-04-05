ccaef40e290cc4cf6c80a967cc9789985a0ee0c5
RevaGee <revoryoutub@gmail.com>
2024-04-05 12:34:00 +0300

    img change

--- src/components/Crm/index.tsx
+++ src/components/Crm/index.tsx
@@ -1,18 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import './styles.scss'
 import '../../index.css'
 import CRM_black from '../../images/icons/Crm_black.png'
 import CRM_grey from '../../images/icons/Crm_grey.png'
-import {useTheme} from "../DarkMode";
+
+import CRM_black_small from '../../images/icons/Crm_black_small.png'
+import CRM_grey_small from '../../images/icons/CRM_grey_small.png'
+
+import { useTheme } from "../DarkMode";
+
 export const Crm = () => {
     const { darkMode } = useTheme();
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <div className='crm'>
             <div className='crm_media'>
                 <h1>Optimize <span>non-stop</span><br/>Check your profits
                     <span className='text_ml'>in a minute</span></h1>
-                <img src={darkMode ? CRM_grey : CRM_black} alt=''/>
+                { windowWidth < 768 ? (
+                    darkMode ? (
+                        <img src={CRM_grey_small} alt=''/>
+                    ) : (
+                        <img src={CRM_black_small} alt=''/>
+                    )
+                ) : (
+                    darkMode ? (
+                        <img src={CRM_grey} alt=''/>
+                    ) : (
+                        <img src={CRM_black} alt=''/>
+                    )
+                )}
             </div>
         </div>
     );
