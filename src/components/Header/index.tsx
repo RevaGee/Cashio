ee02d77e2938732c4a30937340bb652c8eab94da
RevaGee <revoryoutub@gmail.com>
2024-04-05 16:15:57 +0300

    try header 3.0

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -85,9 +85,6 @@ export const Header = () => {
     return (
         <div
             className={open ? 'mobile_header' : 'header'}
-            style={{
-                backgroundColor: showScroll ? `var(--header_color)` : 'transparent',
-            }}
         >
             <div className="media">
                 <div className="left_side">
