1b64be1a07fcfb8e4ff1ddff35aac3a9a1ccd9f8
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-04-06 21:15:27 +0300

    corrected crm animation

--- src/components/Crm/index.tsx
+++ src/components/Crm/index.tsx
@@ -7,6 +7,9 @@ import third from '../../images/3.png'
 import fourth from '../../images/30.png'
 import fifth from '../../images/60.png'
 import fifthPhone from '../../images/4.png'
+import {ContainerScroll} from "../ui/container-scroll-animation";
+import { motion } from "framer-motion";
+import {useInView} from "react-intersection-observer";
 
 
 export const Crm = () => {
@@ -26,25 +29,114 @@ export const Crm = () => {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
+    const [ref_crm_text, inView_crm_text] = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+    const [ref_crm_1, inView_crm_1] = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+    const [ref_crm_2, inView_crm_2] = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+    const [ref_crm_3, inView_crm_3] = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+    const [ref_crm_4, inView_crm_4] = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+    const [ref_crm_5, inView_crm_5] = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+    const [ref_crm_6, inView_crm_6] = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function to remove the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <div className='crm'>
             <div className='crm_media'>
-                <h1>Optimize <span>non-stop</span><br/>Check your profits
-                    <span className='text_ml'>in a minute</span>
-                </h1>
+                <motion.h1
+                    ref={ref_crm_text}
+                    initial={{opacity: 0, x: 0, y: 0}}
+                    animate={{opacity: inView_crm_text ? 1 : 0, x: inView_crm_text ? (width < 768 ? 0 : 0) : -130}}
+                    transition={{duration: 1}}
+                >
+                    Optimize <span>non-stop</span><br/>Check your profits<span className='text_ml'>in a minute</span>
+                </motion.h1>
                 <div className='crm_collage'>
                     <div className='top_collage'>
-                        <img className = 'collage_bar' src={first} alt=''/>
+                        <motion.img
+                            ref={ref_crm_1}
+                            className = 'collage_bar'
+                            src={first} alt=''
+                            initial={{opacity: 0, x: 0}}
+                            animate={{opacity: inView_crm_1 ? 1 : 0, x: inView_crm_1 ? 0 : -130}}
+                            transition={{duration: 1}}
+                        />
                         <div className='right_top'>
-                            <img src={second} alt='' style={{alignSelf: 'end', marginBottom: '5%'}}/>
-                            <img src={third} alt='' style={{alignSelf: 'end'}}/>
+                            <motion.img
+                                ref={ref_crm_2}
+                                src={second} alt=''
+                                style={{alignSelf: 'end', marginBottom: '5%'}}
+                                initial={{opacity: 0, x: 0, y: 0}}
+                                animate={{opacity: inView_crm_2 ? 1 : 0, x: inView_crm_2 ? 0 : 130}}
+                                transition={{duration: 1}}
+                            />
+                            <motion.img
+                                ref={ref_crm_3}
+                                src={third} alt=''
+                                style={{alignSelf: 'end'}}
+                                initial={{opacity: 0, x: 0}}
+                                animate={{opacity: inView_crm_3 ? 1 : 0, x: inView_crm_3 ? 0 : 130}}
+                                transition={{duration: 1}}
+                            />
                         </div>
                     </div>
                     <div className='bottom_collage'>
-                        <img src={fourth} alt='' style={{marginRight: '5%', width: '60%'}}/>
-                        <img className = 'fifth' src={fifth} alt='' style={{width: '35%'}}/>
-                        <img className = 'fifth_phone' src={fifthPhone} alt=''/>
+                        <motion.img
+                            ref={ref_crm_4}
+                            src={fourth} alt=''
+                            style={{marginRight: '5%', width: '60%'}}
+                            initial={{opacity: 0, y: 0, x: 0}}
+                            animate={{opacity: inView_crm_4 ? 1 : 0, y: inView_crm_4 ? 0 : 130, x: inView_crm_4 ? 0 : -130}}
+                            transition={{duration: 1}}
+                        />
+                        <motion.img
+                            ref={ref_crm_5}
+                            className = 'fifth'
+                            src={fifth} alt='' style={{width: '35%'}}
+                            initial={{opacity: 0, y: 0, x: 0}}
+                            animate={{opacity: inView_crm_5 ? 1 : 0, y: inView_crm_5 ? 0 : 130, x: inView_crm_5 ? 0 : 130}}
+                            transition={{duration: 1}}
+                        />
+                        <motion.img
+                            ref={ref_crm_6}
+                            className = 'fifth_phone'
+                            src={fifthPhone} alt=''
+                            initial={{opacity: 0, x: 0}}
+                            animate={{opacity: inView_crm_6 ? 1 : 0, x: inView_crm_6 ? 0 : 130}}
+                            transition={{duration: 1}}
+                        />
                     </div>
                 </div>
             </div>
