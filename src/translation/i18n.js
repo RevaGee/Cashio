6e95fc40427c91bef55f62f6f8c659d494542996
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-04-08 16:43:22 +0300

    back

--- src/translation/i18n.js
+++ src/translation/i18n.js
@@ -2,8 +2,8 @@ import en from './en.json';
 import ru from './ru.json';
 import ua from './ua.json';
 
-import { initReactI18next } from 'react-i18next';
-import i18n from 'i18next';
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
 
 const resources = {
     en: {
@@ -14,26 +14,14 @@ const resources = {
     },
     ua: {
         translation: ua,
-    },
-};
-
-// Зчитуємо значення мови з localStorage
-const savedLanguage = localStorage.getItem('language');
+    }
+}
 
 i18n
     .use(initReactI18next)
     .init({
         resources,
-        lng: savedLanguage || 'en', // Використовуємо збережене значення мови або англійську, якщо немає збереженого значення
-        fallbackLng: 'en',
-    });
-
-// Прослуховуємо подію зміни мови і зберігаємо нове значення у localStorage
-i18n.on('languageChanged', (lng) => {
-    localStorage.setItem('language', lng);
-});
-
-// Зберігаємо список доступних мов у localStorage
-localStorage.setItem('availableLanguages', JSON.stringify(Object.keys(resources)));
-
+        lng: localStorage.getItem('language') || 'en',
+        fallbackLng: 'en'
+    })
 export default i18n;
