705ce29ba4ac625ba1672788ccd2487e994a46bf
RevaGee <revoryoutub@gmail.com>
2024-04-08 23:35:07 +0300

    smt1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './styles.scss';
-import {ArrowUp} from '../../images/icons/ArrowUp';
-import {LogoFooter} from '../../images/icons/iconizer-CASHIO_logo_green';
+import { ArrowUp } from '../../images/icons/ArrowUp';
+import { LogoFooter } from '../../images/icons/iconizer-CASHIO_logo_green';
 import DarkMode from '../DarkMode/DarkMode';
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import i18n from '../../translation/i18n';
 import ParticlesComponent from "../Paralax/particles";
 
-
 export const Header = () => {
+    const headerRef = useRef<HTMLDivElement>(null);
     const [open, setOpen] = useState(false);
     const [languages, setLanguages] = useState(false);
     const [isChecked, setIsChecked] = useState(false);
@@ -18,8 +18,7 @@ export const Header = () => {
     const [darkMode, setDarkMode] = useState(
         localStorage.getItem('darkMode') === 'true'
     );
-
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const [showScroll, setShowScroll] = useState(false);
     const checkScrollTop = () => {
         if (!showScroll && window.pageYOffset > 50) {
@@ -40,16 +39,29 @@ export const Header = () => {
         window.dispatchEvent(new Event('resetDarkMode'));
     }, [darkMode]);
 
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (headerRef.current && !headerRef.current.contains(event.target as Node)) {
+            setOpen(false);
+            setIsChecked(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [headerRef]);
+
     const handleLanguageChange = async (language: string) => {
         window.location.reload();
         window.localStorage.setItem('language', language);
-
         setTimeout(() => {
             setSelectedLanguage(language);
             i18n.changeLanguage(language);
         }, 250);
     };
-
 
     const handleTelegramLink = () => {
         window.open('https://t.me/Cashio_Mngr', '_blank');
@@ -57,12 +69,7 @@ export const Header = () => {
 
     const handleClick = () => {
         setOpen(!open);
-        if (!open) {
-            setIsChecked(true)
-        } else {
-            setIsChecked(false)
-        }
-
+        setIsChecked(!isChecked);
     };
 
     const handleLanguageClick = () => {
@@ -77,29 +84,28 @@ export const Header = () => {
         e.preventDefault();
         const id = sectionId.replace('#', '');
         const element = document.getElementById(id);
-
         if (element) {
             const topOffset = 100;
             const topPos = element.getBoundingClientRect().top + window.pageYOffset - topOffset;
-            window.scrollTo({top: topPos, behavior: 'smooth'});
-            setOpen(false)
+            window.scrollTo({ top: topPos, behavior: 'smooth' });
+            setOpen(false);
+            setIsChecked(false);
         }
-        setIsChecked(false);
-
     }
 
     return (
         <div
+            ref={headerRef}
             className={open ? 'mobile_header' : 'header'}
             style={{
                 backgroundColor: showScroll ? `var(--header_color)` : 'transparent',
             }}
         >
-            <ParticlesComponent/>
+            <ParticlesComponent />
             <div className="media">
                 <div className="left_side">
                     <div className="logo">
-                        <LogoFooter/>
+                        <LogoFooter />
                     </div>
                     <div className="nav_block">
                         <nav className={open ? 'show_menu' : 'navigation'}>
@@ -108,27 +114,22 @@ export const Header = () => {
                                     <a href="#Slogan" onClick={(e) => scrollToSection(e, 'Slogan')}>{t('Features')}</a>
                                 </li>
                                 <li>
-                                    <a href="#Collages"
-                                       onClick={(e) => scrollToSection(e, 'Collages')}>{t('Why Us')}</a>
+                                    <a href="#Collages" onClick={(e) => scrollToSection(e, 'Collages')}>{t('Why Us')}</a>
                                 </li>
                                 <li>
-                                    <a href="#AboutUs"
-                                       onClick={(e) => scrollToSection(e, 'AboutUs')}>{t('About Us')}</a>
+                                    <a href="#AboutUs" onClick={(e) => scrollToSection(e, 'AboutUs')}>{t('About Us')}</a>
                                 </li>
                                 <li>
                                     <a href="#Pricing" onClick={(e) => scrollToSection(e, 'Pricing')}>{t('Pricing')}</a>
                                 </li>
                                 <li>
-                                    <a href="#contacts"
-                                       onClick={(e) => scrollToSection(e, 'contact')}>{t('Contacts')}</a>
+                                    <a href="#contacts" onClick={(e) => scrollToSection(e, 'contact')}>{t('Contacts')}</a>
                                 </li>
                                 <li>
-                                    <a href="#Questions"
-                                       onClick={(e) => scrollToSection(e, 'Questions')}>{t('Questions')}</a>
+                                    <a href="#Questions" onClick={(e) => scrollToSection(e, 'Questions')}>{t('Questions')}</a>
                                 </li>
                                 <li>
-                                    <a href="#for_whom"
-                                       onClick={(e) => scrollToSection(e, 'for_whom')}>{t('ForWhom')}</a>
+                                    <a href="#for_whom" onClick={(e) => scrollToSection(e, 'for_whom')}>{t('ForWhom')}</a>
                                 </li>
                             </ul>
                         </nav>
@@ -142,14 +143,14 @@ export const Header = () => {
                                     {selectedLanguage.toUpperCase()}
                                     <span
                                         className="material-icons dropdown-icon"
-                                        style={languages ? {transform: 'rotate(180deg)'} : {}}
+                                        style={languages ? { transform: 'rotate(180deg)' } : {}}
                                     >
-                    <ArrowUp/>
-                  </span>
+                                        <ArrowUp />
+                                    </span>
                                 </a>
                                 <ul
                                     className="drop-down"
-                                    style={languages ? {display: 'block', justifyContent: 'center'} : {}}
+                                    style={languages ? { display: 'block', justifyContent: 'center' } : {}}
                                 >
                                     <li
                                         onClick={() => {
@@ -184,9 +185,9 @@ export const Header = () => {
                     <button className="demo" onClick={handleTelegramLink}>
                         {t('Get a free demo')}
                     </button>
-                    <DarkMode darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+                    <DarkMode darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
                     <label className="burger">
-                        <input type="checkbox" checked={isChecked} name=""/>
+                        <input type="checkbox" checked={isChecked} name="" />
                         <div className="bar" onClick={handleClick}>
                             <span className="top"></span>
                             <span className="middle"></span>
